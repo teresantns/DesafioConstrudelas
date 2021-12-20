8269b5674e1f7d6bec3ac2e24e91dc47f1827884
Teresa Seabra Antunes <tseabra.antunes@gmail.com>
2021-12-20 16:16:26 -0300

    creating models.py (#2)

--- loyalty_program/apps/referral/models.py
+++ loyalty_program/apps/referral/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.db import models
+from localflavor.br.models import BRCPFField
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=255, blank=False, verbose_name='Nome')
+    cpf = BRCPFField('CPF ', blank=False, primary_key=True,
+                     help_text='Formato: 00011122233')
+    phone = models.CharField(max_length=11, blank=False,
+                             help_text='Formato DDD + Número', verbose_name='Telefone')
+    email = models.EmailField(max_length=255, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    points = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        details = f'Cliente: {self.name} | cpf: {self.cpf}'
+        return details
+
+
+class Referral(models.Model):
+    source_cpf = BRCPFField('CPF do usuário de origem',
+                            blank=False, unique=False)
+    target_cpf = BRCPFField('CPF do usuário de destino',
+                            blank=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        details = f'Indicador: {self.source_cpf} | Indicado: {self.target_cpf}'
+        return details
