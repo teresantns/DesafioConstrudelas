2dc96cc2311ee4376a886cb5a1f56812b1f6e51d
Teresa Seabra Antunes <tseabra.antunes@gmail.com>
2021-12-21 23:56:39 -0300

    cleaned tests.py file

--- loyalty_program/apps/referral/tests.py
+++ loyalty_program/apps/referral/tests.py
@@ -1,9 +1,3 @@
-from django.shortcuts import get_object_or_404
 from django.test import TestCase
-from .models import Client
 
 # Create your tests here.
-
-cpf = '12531049675'
-is_client_on_db = get_object_or_404(Client, cpf=cpf)
-print(is_client_on_db)
