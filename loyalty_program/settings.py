958fa2c883bd4fddcea14058c44d1aaac3946d12
Teresa Antunes <92113896+teresantns@users.noreply.github.com>
2022-01-18 15:28:13 -0300

    security update

--- loyalty_program/settings.py
+++ loyalty_program/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qs+mjd645%ayzg5*c#s4*6e)2=)i6@iicy!d-zvc_cl#zm*0mq'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
